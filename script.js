cd3029fea446084243d1f15924af147856766106
Keshav Sharma <Keshav.bhambi@gmail.com>
2025-04-30 10:01:43 +0530

    revision of type coercion completed

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ console.log(userAge3 == userAge4);//true
 //strict equality operator
 console.log(userAge3 === userAge4);//false
 // for comparison purposes, always use strict equality operator(===)
+
+const userAge5 = '21';// here basically behind the scenes V8 engine is converting this string into a number for effective comparison.This concept is basically called type coercion.
+const userAge6 = 21;
+console.log(userAge5 == userAge6);
+console.log(userAge5 === userAge6);
+// Basically in the strict equality operator, we can do the manual conversion
+console.log(+userAge5 === userAge6);//true
+console.log(parseInt(userAge5) === userAge6);
+console.log('' + userAge6 === userAge5);
